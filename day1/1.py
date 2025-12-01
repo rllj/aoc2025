0c555b00514d265cd188e573e80c2b1a6ce5debb
Ruben Lee Landrø Johnsen <RubenLee.Johnsen@skatteetaten.no>
2025-12-01 12:47:27 +0100

    update python to be part one

--- day1/1.py
+++ day1/1.py
@@ -6,15 +6,12 @@ result = 0
 
 for line in sys.stdin:
     if line[0] == 'L':
-        amount = int(line[1:])
-        if cnt == amount:
-            result += 1
-        elif cnt - amount <= 0:
-            result += (amount - cnt) // 100
-        cnt = (cnt - amount) % 100
+        cnt = (cnt - int(line[1:])) % 100
     else:
-        result += (cnt + amount) // 100
-        cnt = (cnt + amount) % 100
+        cnt = (cnt + int(line[1:])) % 100
+
+    if cnt == 0:
+        result += 1
 
 print(result)
 
